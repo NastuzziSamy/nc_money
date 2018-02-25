5cba536eb417d2a3fc92dbf7859afe672081c1cb
powerpaul17 <powerpaul17@users.noreply.github.com>
2018-02-25 17:22:41 +0100

    moved aurelia files to correct place

--- templates/main.php
+++ templates/main.php
@@ -1,6 +1,4 @@
-<?php
-	script('money', 'aurelia/scripts/vendor-bundle');
-?>
-
-<!-- <script src="scripts/vendor-bundle.js"></script> -->
-<script>SystemJS.import("aurelia-bootstrapper").catch(function(err) { console.log(err); })</script>
+<div aurelia-app="main">
+  <script src="scripts/vendor-bundle.js"></script>
+  <script>SystemJS.import("aurelia-bootstrapper").catch(function(err) { console.log(err); })</script>
+</div>
